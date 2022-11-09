2fd1887371e9b110b058cf785078a44016508d8f
Rati1324 <61045363+Rati1324@users.noreply.github.com>
2022-11-09 13:20:57 +0400

    Delete temp.py

--- temp.py
+++
@@ -1,9 +0,0 @@
-import json
-test_str = "[{'Gfg' : 3, 'Best' : 8}, {'Gfg' : 4, 'Best' : 9}]"
-res = json.loads(test_str.replace("'", '"')) 
-
-x = {"name":"john", "age":23}
-lx = [x for i in range(9)]
-
-x = [3,4]
-print(x.max())
