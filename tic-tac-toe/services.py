eaf1532b407663b19e192410ca71cfe7bc4b19b8
Rati1324 <rati.mtsituri@gau.edu.ge>
2022-11-09 12:23:09 +0400

    fixed check function

--- tic-tac-toe/services.py
+++ tic-tac-toe/services.py
@@ -26,7 +26,7 @@ def make_move(db: sqlalchemy.orm.Session, game_id: str, move: schemas.Move):
     last_play = max([int(i) for i in db_game.order])
 
     # null = in_progress
-    if db_game.winner != "null":
+    if db_game.winner != "in_progress":
         return {"result": "error", "error_code":"game_is_finished"}
 
     if db_game.board[move.position] == '-' and 0 <= move.position <= 8:
@@ -38,10 +38,9 @@ def make_move(db: sqlalchemy.orm.Session, game_id: str, move: schemas.Move):
         cur_order[move.position] = str(last_play+1)
         db_game.order = ''.join(cur_order)
 
+        db_game.winner = check_game(db_game.board)
         db.commit()
         db.refresh(db_game)
-
-        check(db, game_id=game_id)
         return {"result": "success"}
     else:
         return {"result": "error", "error_code":"invalid_position"}
@@ -50,11 +49,11 @@ def check(db: sqlalchemy.orm.Session, game_id: str):
     db_game = db.query(models.Game).filter(models.Game.id == game_id).first()
     result = db_game.winner
 
-    if result == "null":
-        return {"game": "in_progress"}
+    if result in "X0null":
+        return {"game": "finished", "winner": result}
     else:
-        return {"game": "finished", "winner": result if result != "draw" else "null"}
-
+        return {"game": result}
+    
 def check_game(board):
     positions = [[0, 1, 2],
                 [3, 4, 5],
@@ -63,19 +62,19 @@ def check_game(board):
                 [1, 4, 7],
                 [2, 5, 8],
                 [0, 4, 8],
-                [2, 4, 6],]
+                [2, 4, 6]]
 
-    draw = True
-    winner = "null"
+    status = "null"
 
     for i in positions:
         a, b, c = i
         if board[a] != '-' and board[b] != '-' and board[c] != '-':
             if board[a] == board[b] and board[b] == board[c]:
-                return board[a]
+                status = board[a]
+                break
         else:
-            draw = False
-    return "draw" if draw else winner
+            status = "in_progress"
+    return status
 
 def get_history(db: sqlalchemy.orm.Session):
     db_games =  db.query(models.Game).all()
